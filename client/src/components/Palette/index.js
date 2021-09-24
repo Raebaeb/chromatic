8d2e72c77fbc30cfa9b1d6bf85916d5749ce6e09
rachelperez <rlb2rlp@gmail.com>
2021-09-23 17:54:56 -0700

    adjust style classes to better suit all sizes

--- client/src/components/Palette/index.js
+++ client/src/components/Palette/index.js
@@ -12,12 +12,10 @@ function Palette(props) {
   }
   return (
     <container className="palette-card">
-      <section className="palette-info">
         <h2>{palette}</h2>
         <h4>{username}</h4>
         <Link to={`/edit/${props.palette.id}`}><button className='edit-btn'>edit</button></Link>
         <button className='delete-btn' onClick={handleDelete}>delete</button>
-      </section>
       <section className="palette-colors">
         <article className="color-1" style={{ backgroundColor: `${ hex1 }` }}></article>
         <article className="color-2" style={{ backgroundColor: `${ hex2 }` }}></article>
