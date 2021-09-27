0aa74347c61fd606fb818d9405cd4e659b992779
rachelperez <rlb2rlp@gmail.com>
2021-09-26 23:23:52 -0700

    adjust logo svg

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
-import logo from '../../assets/logo.svg'
+import logoMin from '../../assets/logoMin.svg'
 
 const Nav = () => {
   return (
@@ -10,7 +10,7 @@ const Nav = () => {
         <Link to='/new'>create palette</Link>
       </nav>
         <Link to='/' id='logo-link'>
-          <img src={logo} id='logo' alt='chromatica logo'/>
+          <img src={logoMin} id='logo' alt='chromatica logo'/>
         </Link>
     </header>
   );
